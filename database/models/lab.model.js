f0664c30fc90cd912820b4fb9f72450510af29fd
Ahmedzayed27 <99611506+Ahmedzayed27@users.noreply.github.com>
2023-03-03 18:50:08 +0200

    updating

---
+++ database/models/lab.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const labSchema = new mongoose.Schema({
+name:String,
+price:String,
+category:String,
+date:Date,
+patientId :{
+    type : mongoose.Types.ObjectId,
+    ref:"User"
+},
+doctorId :{
+    type :mongoose.Types.ObjectId,
+    ref:"User"
+}
+}
+)
+
+
+const labModel = mongoose.model('lab',labSchema)
+
+export default labModel;
---
+++ src/modules/lab/lab.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as labController from './controller/lab.controller.js'
+const router = express.Router();
+
+router.post("/addlab",labController.addLab)
+router.get("/getlab",labController.getLab)
+router.put("/updatelab",labController.updateLab)
+router.delete("/deletelab",labController.deleteLab)
+
+
+
+
+export default router;
