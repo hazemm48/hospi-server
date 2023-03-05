19d32ab54af2490b56fe7170d7bfd85fc9a542eb
Hazem Mohamed <hazem1abc@gmail.com>
2023-03-05 17:14:03 +0200

    last

--- src/modules/users/patient/controller/patient.controller.js
+++ src/modules/users/patient/controller/patient.controller.js
@@ -154,41 +154,6 @@ const buyMedicine = async (req, res) => {
   }
 };
 
-//reserve lab
-const reserveLab = async (req, res) => {
-  let all = req.body;
-  all.patientId = req.userId;
-  try {
-    const reservedlab = await labModel.insertMany(all);
-    res.json({ message: "reservation success", reservedlab });
-  } catch (error) {
-    res.json({ message: "error", error });
-  }
-};
-
-//getReserve Lab
-
-const getReserveLab = async (req, res) => {
-  let all = req.body;
-  try {
-    if (all.oper == "all") {
-      const allReservedLab = await labModel.find({ patientId: req.userId });
-      res.json({ message: "all reservations", allReservedLab });
-    } else if (all.oper == "one") {
-      const reserved = await labModel
-        .findById(all._id)
-        .populate("patientId");
-      if (reserveLab.patientId._id == req.userId) {
-        res.json({ message: "reservation found", reserved });
-      } else {
-        res.json({ message: "not authorized" });
-      }
-    }
-  } catch (error) {
-    res.json({ message: "error", error });
-  }
-};
-
 
 export {
   getPatient,
@@ -201,6 +166,4 @@ export {
   getReserveLab,
   medicReport,
   buyMedicine,
-  getReserveLab,
-  reserveLab
 };
