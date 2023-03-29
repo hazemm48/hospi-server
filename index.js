2026ecb5c9a73427725d2b24a0b7a2e0335969d6
Ahmedzayed27 <99611506+Ahmedzayed27@users.noreply.github.com>
2023-03-29 22:26:27 +0200

    morgan

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const app = express()
 import { connection } from "./database/connection.js"
 import mainRoutes from './src/modules/main.routes.js'
 import { globalError } from "./src/services/asyncHandler.js"
+import morgan from 'morgan'
 
 
 const server = http.createServer(app);
 
 
 
-
+app.use(morgan("combined"))
 app.use(globalError)
 app.use(cors());
 app.use(express.json())
