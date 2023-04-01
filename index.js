1d9200df127a7ea4500941f5118eb2eccc716963
Hazem Mohamed <hazem1abc@gmail.com>
2023-04-01 03:08:44 +0200

    d

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const app = express()
 import { connection } from "./database/connection.js"
 import mainRoutes from './src/modules/main.routes.js'
 
+app.use(cors());
 app.use(express.json())
 app.use('/uploads',express.static("uploads"));
 connection()
 app.use('/api/v1',mainRoutes)
-app.get('/',(req,res)=> res.send("hellooo"))
+app.get('/',(req,res)=> res.send("running"))
 app.listen(3000,()=> console.log('listening'))
